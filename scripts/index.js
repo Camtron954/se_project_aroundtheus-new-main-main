98c5ca2d165d42ea712ec49cccf8d85c125d834c
Cam-tron <Camtron954@gmail.com>
2024-10-31 14:31:11 -0400

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector('#profile-edit-modal');
+
+const closeButton = profileEditModal.querySelector('modal__close');
+
+profileEditButton.addEventListener('click', () => {
+    profileEditModal.classList.add('modal_opened');
+});
+
+closeButton.addEventListener('click', function() {
+  profileEditModal.classList.remove('modal_opened');  
+});
+
+
